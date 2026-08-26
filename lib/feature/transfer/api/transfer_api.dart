12d819ce7537a926e178adfa99994175fdf3b559
Hamidreza Pakpour <136631329+Hamrez95@users.noreply.github.com>
2026-08-26 18:29:35 +0330

    transfer: export live capability observation contract (#48)

--- lib/feature/transfer/api/transfer_api.dart
+++ lib/feature/transfer/api/transfer_api.dart
@@ -16,6 +16,7 @@ export '../domain/entity/session_role.dart';
 export '../domain/entity/transfer_mode.dart';
 export '../domain/entity/transfer_mode_analytics.dart';
 export '../domain/entity/transport_capability_advertisement.dart';
+export '../domain/entity/transport_capability_observation.dart';
 export '../domain/entity/transport_stats.dart';
 export '../domain/entity/waki_packet.dart';
 export '../domain/entity/wifi_hotspot_segment.dart';
