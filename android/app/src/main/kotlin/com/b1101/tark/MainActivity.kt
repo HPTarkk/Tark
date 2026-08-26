642d6d195d00f69e85b2c68aac9f017cc19af689
Hamidreza Pakpour <136631329+Hamrez95@users.noreply.github.com>
2026-08-26 17:35:53 +0330

    feat: register transport capability platform bridge

--- android/app/src/main/kotlin/com/b1101/tark/MainActivity.kt
+++ android/app/src/main/kotlin/com/b1101/tark/MainActivity.kt
@@ -10,6 +10,7 @@ import com.b1101.tark.hotspot.HotspotHandler
 import com.b1101.tark.hotspot.WifiJoinHandler
 import com.b1101.tark.keepalive.KeepAliveHandler
 import com.b1101.tark.network.NetworkBindingHandler
+import com.b1101.tark.network.TransportCapabilityHandler
 import com.b1101.tark.widget.WidgetControlBridge
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
@@ -87,6 +88,11 @@ class MainActivity : FlutterActivity() {
             flutterEngine.dartExecutor.binaryMessenger,
             NetworkBindingHandler.METHOD_CHANNEL,
         ).setMethodCallHandler(networkBinding)
+
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            TransportCapabilityHandler.METHOD_CHANNEL,
+        ).setMethodCallHandler(TransportCapabilityHandler(applicationContext))
 
         val keepAlive = KeepAliveHandler(
             applicationContext,
