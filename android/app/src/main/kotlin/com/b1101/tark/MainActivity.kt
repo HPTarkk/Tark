beea4566071f08b384161cbb0d724a3e54e7be7d
Hamidreza Pakpour <136631329+Hamrez95@users.noreply.github.com>
2026-08-27 22:35:00 +0330

    android: register Room identity secure-store channel (#161)

--- android/app/src/main/kotlin/com/b1101/tark/MainActivity.kt
+++ android/app/src/main/kotlin/com/b1101/tark/MainActivity.kt
@@ -11,6 +11,7 @@ import com.b1101.tark.hotspot.WifiJoinHandler
 import com.b1101.tark.keepalive.KeepAliveHandler
 import com.b1101.tark.network.NetworkBindingHandler
 import com.b1101.tark.network.TransportCapabilityHandler
+import com.b1101.tark.security.RoomIdentitySecureStoreHandler
 import com.b1101.tark.widget.WidgetControlBridge
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
@@ -93,6 +94,11 @@ class MainActivity : FlutterActivity() {
             flutterEngine.dartExecutor.binaryMessenger,
             TransportCapabilityHandler.METHOD_CHANNEL,
         ).setMethodCallHandler(TransportCapabilityHandler(applicationContext))
+
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            RoomIdentitySecureStoreHandler.METHOD_CHANNEL,
+        ).setMethodCallHandler(RoomIdentitySecureStoreHandler(applicationContext))
 
         val keepAlive = KeepAliveHandler(
             applicationContext,
